946117a5968247810f8284baa2b3e3a730b08759
Sheridan Zivanovich <sdzivanovich@gmail.com>
2015-03-01 17:04:06 -0500

    create JUnit test case files for user stories

---
+++ cs1699/zivanovicdonald/tests/EditArticlesTests.java
@@ -0,0 +1,16 @@
+package cs1699.zivanovicdonald.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EditingArticles
+{
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
