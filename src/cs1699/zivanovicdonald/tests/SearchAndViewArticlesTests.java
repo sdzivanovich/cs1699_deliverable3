6980db7082050db21b5e5574137c263b1e0571a0
Sheridan Zivanovich <sdzivanovich@gmail.com>
2015-03-01 19:29:35 -0500

    Change some searching scenarios, add more unit tests for scenarios

--- src/cs1699/zivanovicdonald/tests/SearchAndViewArticlesTests.java
+++ src/cs1699/zivanovicdonald/tests/SearchAndViewArticlesTests.java
@@ -1,6 +1,7 @@
 package cs1699.zivanovicdonald.tests;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -38,7 +39,48 @@ public class SearchAndViewArticlesTests
         _driver.findElement(By.id("searchButton")).click();
      // Then the user should be taken to the article's page
         assertEquals("https://en.wikipedia.org/wiki/Cat", _driver.getCurrentUrl());
+        assertEquals("Cat", _driver.findElement(By.id("firstHeading")).getText());
     }
+    
+    @Test
+    //Scenario: Searching non-existent article
+    public void searchForNonExistentArticle()
+    {
+        // Given the user is on the main page
+        _driver.get("https://en.wikipedia.org/wiki/Main_Page");
+        // And a phrase that is not the name of an article
+        String target = "rent a cat";
+        // When the user searches for the phrase
+        _driver.findElement(By.id("searchInput")).click();
+        _driver.findElement(By.id("searchInput")).clear();
+        _driver.findElement(By.id("searchInput")).sendKeys(target);
+        _driver.findElement(By.id("searchButton")).click();
+        // Then the user should see that an article for the phrase does not exist
+        assertThat(_driver.findElement(By.cssSelector("i")).getText(),containsString("The page \"Rent a cat\" does not exist."));
+        //And the user should see search results for the phrase        
+        assertEquals("Search results", _driver.findElement(By.id("firstHeading")).getText());
+    }
+    
+    @Test
+    //Scenario: Viewing categories of articles
+    public void searchForCategory()
+    {
+        // Given the user is on the main page
+        _driver.get("https://en.wikipedia.org/wiki/Main_Page");
+        // And the name of a category
+        String category = "Category:Religion";
+        // When the user searches for the category
+        _driver.findElement(By.id("searchInput")).click();
+        _driver.findElement(By.id("searchInput")).clear();
+        _driver.findElement(By.id("searchInput")).sendKeys(category);
+        _driver.findElement(By.id("searchButton")).click();
+        // Then the user should see the page for the category   
+        assertEquals(category, _driver.findElement(By.id("firstHeading")).getText());
+        // And the user should see links to pages in the category
+        assertEquals("Atheism and religion", _driver.findElement(By.linkText("Atheism and religion")).getText());
+    }
+    
+    
 
     @After
     public void tearDown() throws Exception 
