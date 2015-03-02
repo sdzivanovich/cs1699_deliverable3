12064184a5b2ed6175e981d705834a8ed53b9741
ram757 <rmcdonald144@gmail.com>
2015-03-02 12:18:17 -0500

    Added Community Tests
    Added community tests and modified some formatting issues in
    EditArticlesTests.java

--- src/cs1699/zivanovicdonald/tests/CommunityPortalTests.java
+++ src/cs1699/zivanovicdonald/tests/CommunityPortalTests.java
@@ -2,15 +2,201 @@ package cs1699.zivanovicdonald.tests;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+
+/* Feature:
+ * As a user (with or without an account)
+ * I want to interact with others by asking questions and viewing helpful content
+ * So that I can have a much easier time navigating Wikipedia
+ */
 public class CommunityPortalTests
 {
+    private WebDriver driver;
+    private String baseUrl;
+
+    @Before
+    public void setUp()
+    {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        baseUrl = "http://www.wikipedia.org/";
+
+    }//END setUp()
+
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+
+    }//END tearDown()
 
     @Test
-    public void test()
+    //Scenario: Community Help Desk
+    public void testHelpDesk()
     {
-        fail("Not yet implemented");
-    }
+        //Given a user
+        driver.get(baseUrl + "/");
+        driver.findElement(By.cssSelector("strong")).click();
+        driver.findElement(By.cssSelector("a[title=\"About the project, what you can do, where to find things\"]")).click();
+
+        //When the user needs help regarding how to use the site
+        driver.findElement(By.linkText("Help desk")).click();
+        driver.findElement(By.cssSelector("div.center")).click();
+
+        //Then the user should be able to navigate to the Help Desk
+        assertEquals("Wikipedia:Help desk", driver.findElement(By.id("firstHeading")).getText());
+
+        //And be able to click to post a new question
+        assertEquals("Click here to ask a new question about how to use or edit Wikipedia »", driver.findElement(By.cssSelector("span > b")).getText());
+        driver.findElement(By.cssSelector("span > b")).click();
+
+        //And then be able to click "Save page" after typing their question to post it
+        assertEquals("Editing Wikipedia:Help desk (new section)", driver.findElement(By.id("firstHeading")).getText());
+        assertTrue(isElementPresent(By.id("wpSave")));
+        assertEquals("", driver.findElement(By.id("wpSave")).getText());
+
+    }//END testHelpDesk()
+
+    @Test
+    //Scenario: Community Reference Desk
+    public void testReferenceDesk() 
+    {
+        //Given a user at the front page
+        driver.get(baseUrl + "/");
+        driver.findElement(By.cssSelector("strong")).click();
+        driver.findElement(By.id("searchInput")).click();
+        driver.findElement(By.cssSelector("a[title=\"About the project, what you can do, where to find things\"]")).click();
+
+        //When the user needs help regarding a particular subject matter
+        driver.findElement(By.linkText("Reference desk")).click();
+
+        //Then the user should be able to navigate to the Reference Desk
+        assertEquals("Wikipedia:Reference desk", driver.findElement(By.id("firstHeading")).getText());
+
+        //And then be able to click a category for their question
+        assertEquals("Computers and IT", driver.findElement(By.linkText("Computers and IT")).getText());
+        driver.findElement(By.linkText("Computers and IT")).click();
+
+        //And the user should be able to click to post a new question
+        assertTrue(isElementPresent(By.name("create")));
+        driver.findElement(By.name("create")).click();
+
+        //And be able to click "Save page" after typing their qeustion to post it
+        assertTrue(isElementPresent(By.id("wpSave")));
+
+    }//END testReferenceDesk()
+
+    @Test
+    //Scenario: Reference Look-Up
+    public void testReferenceDeskLookUp()
+    {
+        //Given a user at the front page
+        driver.get(baseUrl + "/");
+
+        //When the user needs help regarding a particular subject matter
+        driver.findElement(By.cssSelector("strong")).click();
+        driver.findElement(By.cssSelector("a[title=\"About the project, what you can do, where to find things\"]")).click();
+
+        //Then the user should be able to navigate to the Reference Desk
+        assertTrue(isElementPresent(By.linkText("Reference desk")));
+        driver.findElement(By.linkText("Reference desk")).click();
+        assertEquals("Wikipedia:Reference desk", driver.findElement(By.id("firstHeading")).getText());
+        driver.findElement(By.id("bodySearchInputMP")).click();
+
+        //And the user should be able to search Wikipedia as a whole from the Reference Desk
+        assertTrue(isElementPresent(By.id("bodySearchInputMP")));
+        assertTrue(isElementPresent(By.name("go")));
+
+        //And the user should be able to search the reference desk archives specifically
+        assertTrue(isElementPresent(By.cssSelector("p > input[name=\"search\"]")));
+        assertTrue(isElementPresent(By.name("fulltext")));
+
+        driver.findElement(By.cssSelector("p > input[name=\"search\"]")).clear();
+        driver.findElement(By.cssSelector("p > input[name=\"search\"]")).sendKeys("cats");
+        driver.findElement(By.name("fulltext")).click();
+
+        //And searching the archives should return a "Search Results" page
+        assertEquals("Search results", driver.findElement(By.id("firstHeading")).getText());
+
+    }//END testReferenceDeskLookUp()
+
+
+    @Test
+    //Scenario: Community Teahouse
+    public void testTeaHouse() 
+    {
+        //Given a user at the front page
+        driver.get(baseUrl + "/");
+        driver.findElement(By.cssSelector("strong")).click();
+        driver.findElement(By.cssSelector("a[title=\"About the project, what you can do, where to find things\"]")).click();
+        //When the user needs help regarding how to edit pages
+        driver.findElement(By.linkText("Peer editing help")).click();
+
+        //Then the user should be able to navigate to the Teahouse page
+        assertEquals("Wikipedia:Teahouse/Questions", driver.findElement(By.id("firstHeading")).getText());
+
+        //And the user should be able to post a question at the Teahouse
+        assertTrue(isElementPresent(By.cssSelector("span.mw-ui-button.mw-ui-progressive")));
+        driver.findElement(By.cssSelector("span.mw-ui-button.mw-ui-progressive")).click();
+        driver.findElement(By.id("wp-th-question-title")).clear();
+        driver.findElement(By.id("wp-th-question-title")).sendKeys("I like cats");
+        driver.findElement(By.id("wp-th-question-text")).clear();
+        driver.findElement(By.id("wp-th-question-text")).sendKeys("And I like dogs\n\n~~~~");
+
+        //And, once they type their question, they can click "Ask my question" to post it
+        assertTrue(isElementPresent(By.cssSelector("span.ui-button-text")));
+
+    }//END testTeaHouse()
+
+    @Test
+    //Scenario: Community Village Pump
+    public void testVillagePump() 
+    {
+        //Given a user at the front page
+        driver.get(baseUrl + "/");
+        driver.findElement(By.cssSelector("strong")).click();
+
+        //When the user wishes to discuss issues concerning Wikipedia
+        driver.findElement(By.cssSelector("a[title=\"About the project, what you can do, where to find things\"]")).click();
+
+        //Then the user should be able to navigate to the Village pump page
+        assertTrue(isElementPresent(By.linkText("Village pump")));
+        driver.findElement(By.linkText("Village pump")).click();
+
+        //And the user should be able to select a particular category
+        assertTrue(isElementPresent(By.linkText("Miscellaneous")));
+        driver.findElement(By.linkText("Miscellaneous")).click();
+
+        //And the user should be able to create a new post
+        assertTrue(isElementPresent(By.linkText("New post")));
+        driver.findElement(By.linkText("New post")).click();
+
+        //And then the user should be able to click "Save page" to post it
+        assertTrue(isElementPresent(By.id("wpSave")));
+
+    }//END testVillagePump()
+
+    private boolean isElementPresent(By by) 
+    {
+        try 
+        {
+            driver.findElement(by);
+            return true;
+        } 
+        catch (NoSuchElementException e) 
+        {
+            return false;
+        }
+
+    }//END isElementPresent()
+
 
 }
--- src/cs1699/zivanovicdonald/tests/EditArticlesTests.java
+++ src/cs1699/zivanovicdonald/tests/EditArticlesTests.java
@@ -11,6 +11,11 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import static org.hamcrest.CoreMatchers.*;
 
+/* Feature:
+ * As a user ( not logged in)
+ * I want to review and edit articles
+ * so that I can update the contents of an article with new or more-correct information as well as preview such changes.
+ */
 public class EditArticlesTests
 {
 
@@ -18,16 +23,16 @@ public class EditArticlesTests
     private String baseUrl;
 
     @Before
-    public void setUp() throws Exception 
+    public void setUp() 
     {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         baseUrl = "http://www.wikipedia.org/";
 
     }//END setUp()
-    
+
     @After
-    public void tearDown() throws Exception 
+    public void tearDown() 
     {
         driver.quit();
 
@@ -35,7 +40,7 @@ public class EditArticlesTests
 
     @Test
     //Scenario: Semi-Protected Page
-    public void semiProtectedPageTest() throws Exception 
+    public void testSemiProtectedPage() 
     {
         //Given an existing Semi-Protected page
         //And a user that is not logged in
@@ -45,13 +50,13 @@ public class EditArticlesTests
         driver.findElement(By.id("searchInput")).clear();
         driver.findElement(By.id("searchInput")).sendKeys("cat");
         driver.findElement(By.id("searchButton")).click();
-        
+
         //When the user attempts to edit the page
         //Then the user should not be able to click "edit"
         assertFalse(isElementPresent(By.linkText("Edit")));
-        
+
         driver.findElement(By.linkText("View source")).click();
-        
+
         //And the user should see a prompt concerning it being semi-protected in the View Source link
         assertEquals("This page is currently semi-protected so that only established registered users can edit it.", driver.findElement(By.cssSelector("span.mbox-text-span")).getText());
 
@@ -59,7 +64,7 @@ public class EditArticlesTests
 
     @Test
     //Scenario: Non-Protected Page
-    public void nonProtectedPage() throws Exception 
+    public void testNonProtectedPage() 
     {
         //Given an existing Non-Protected page
         //And a user that is not logged in
@@ -68,13 +73,13 @@ public class EditArticlesTests
         driver.findElement(By.id("searchInput")).clear();
         driver.findElement(By.id("searchInput")).sendKeys("almond butter");
         driver.findElement(By.id("searchButton")).click();
-        
+
         //When the user attempts to edit the page
         driver.findElement(By.linkText("Edit")).click();
-        
+
         //Then the user should arrive at the "editing" page
         assertEquals("Editing Almond butter", driver.findElement(By.id("firstHeading")).getText());
-        
+
         //And the user should see a "Save page" button so that they may save their changes
         assertTrue(isElementPresent(By.id("wpSave")));
         assertEquals("Save page", driver.findElement(By.id("wpSave")).getAttribute("value"));
@@ -83,7 +88,7 @@ public class EditArticlesTests
 
     @Test
     //Scenario: Preview Change
-    public void previewChangeTest() throws Exception 
+    public void testPreviewChange() 
     {
         //Given an existing page
         //And a user that is not logged in
@@ -92,7 +97,7 @@ public class EditArticlesTests
         driver.findElement(By.id("searchInput")).clear();
         driver.findElement(By.id("searchInput")).sendKeys("null pointer");
         driver.findElement(By.id("searchButton")).click();
-        
+
         //When the user edits the page
         driver.findElement(By.linkText("Edit")).click();
 
@@ -130,7 +135,7 @@ public class EditArticlesTests
         driver.findElement(By.id("searchInput")).clear();
         driver.findElement(By.id("searchInput")).sendKeys("null pointer");
         driver.findElement(By.id("searchButton")).click();
-        
+
         //When the user edits the page
         driver.findElement(By.linkText("Edit")).click();
 
@@ -169,22 +174,22 @@ public class EditArticlesTests
         driver.get(baseUrl + "/");
         driver.findElement(By.cssSelector("strong")).click();
         driver.findElement(By.id("searchInput")).clear();
-        
+
         //When the user navigates to the page
         driver.findElement(By.id("searchInput")).sendKeys("cats");
         driver.findElement(By.id("searchButton")).click();
         driver.findElement(By.linkText("View history")).click();
-        
+
         //Then the user should arrive at the "Revision history" page
         assertEquals("Cat: Revision history", driver.findElement(By.id("firstHeading")).getText());
-        
+
         //And have the ability to filter the history
         assertTrue(isElementPresent(By.id("mw-history-search")));
         driver.findElement(By.id("year")).clear();
         driver.findElement(By.id("year")).sendKeys("2014");
         driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
         driver.findElement(By.linkText("04:24, 24 October 2014")).click();
-        
+
         //And the user should be able to see the edit history for that particular page
         assertThat(driver.findElement(By.cssSelector("b")).getText(), containsString("This is an old revision of this page"));
 
@@ -204,17 +209,18 @@ public class EditArticlesTests
         driver.findElement(By.id("searchInput")).sendKeys("null");
         driver.findElement(By.id("searchButton")).click();
         driver.findElement(By.linkText("Edit")).click();
-        
+
         //Then the user should arrive at the "editing" page
         assertEquals("Editing Null", driver.findElement(By.id("firstHeading")).getText());
-        
+
         //And the page should explicitly say that the user is editing the disambiguation page
         assertThat(driver.findElement(By.cssSelector("td.mbox-text")).getText(), containsString("This is not an article; this is a disambiguation page, for directing readers quickly to intended articles."));
-        
+
         //And the user should be able to edit that page
         assertTrue(isElementPresent(By.id("wpSave")));
 
     }//END testDisambiguationEdit()
+
 
     private boolean isElementPresent(By by) 
     {
